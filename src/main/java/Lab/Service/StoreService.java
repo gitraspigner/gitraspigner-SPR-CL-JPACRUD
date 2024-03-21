86b7ce4b7fd62b348b56bebf481a012ffd606363
Ravi Spigner <585pieces@protonmail.com>
2024-03-21 21:42:30 +0000

    completed Coding Lab: Spring JPA CRUD

--- src/main/java/Lab/Service/StoreService.java
+++ src/main/java/Lab/Service/StoreService.java
@@ -15,42 +15,48 @@ public class StoreService {
         this.storeRepository = storeRepository;
     }
     /**
-     * TODO: given a transient store, persist the store and return it.
+     * DONE: given a transient store, persist the store and return it.
      * @param store a transient store
      * @return the persisted store
      */
     public Store persistStore(Store store){
-        return null;
+        return storeRepository.save(store);
     }
     /**
-     * TODO: get all store entities
+     * DONE: get all store entities
      * @return all store entities
      */
     public List<Store> getAllStores(){
-        return null;
+        return storeRepository.findAll();
     }
     /**
-     * TODO: given an id of a store, return the store.
+     * DONE: given an id of a store, return the store.
      *
      * @param id id of store entity
      * @return a store entity
      */
     public Store getStoreById(long id){
-        return null;
+        return storeRepository.findById(id).get();
     }
     /**
-     * TODO: given an id of an existing store, delete the store
+     * DONE: given an id of an existing store, delete the store
      */
     public void deleteStore(long id){
-//        code here
+        storeRepository.deleteById(id);
     }
     /**
-     * TODO: given an id and some replacement data for a store, overwrite the data of an existing store,
+     * DONE: given an id and some replacement data for a store, overwrite the data of an existing store,
      * and return the updated store.
      * @return the updated store entity
      */
     public Store updateStore(long id, Store replacement){
-        return null;
+        Store target = storeRepository.findById(id).get();
+        if (target != null) {
+            target.setName(replacement.getName());
+            target.setAddress(replacement.getAddress());
+            
+        }
+        return storeRepository.save(target);
     }
 
 }
